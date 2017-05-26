a900e24daabd8fb145335692112ced2a5db3288c
Lars Göransson <lars.goransson.77@spray.se>
2017-05-26 11:44:46 +0200

    Added specs for put/2, get/2, and get/3

--- spec/ex_model_spec.exs
+++ spec/ex_model_spec.exs
@@ -36,4 +36,93 @@ defmodule ExModel.Spec do
       end
     end
   end
+
+  describe "put/2" do
+    subject do: Subject.put(Subject.new(), attribute(), value())
+    let :value, do: "foo"
+
+    context "when the given attribute has been declared" do
+      let :attribute, do: :foo
+
+      it "assigns the attribute" do
+        expect(Subject.get subject(), attribute()).to eq value()
+      end
+    end
+
+    context "when the given attribute has not been declared" do
+      let :attribute, do: :frozz
+
+      it "raises an exception" do
+        expect(fn -> subject() |> IO.inspect end).to(
+          raise_exception(RuntimeError))
+      end
+    end
+  end
+
+  describe "get/2" do
+    subject do: Subject.new()
+      |> Subject.put(:foo, value())
+      |> Subject.get(attribute())
+
+    let :value, do: "foo"
+
+    context "when the given attribute has been set" do
+      let :attribute, do: :foo
+
+      it "returns the attribute value" do
+        expect(subject()).to eq value()
+      end
+    end
+
+    context "when the given attribute has not been set" do
+      let :attribute, do: :bar
+
+      it "returns nil" do
+        expect(subject()).to be_nil()
+      end
+    end
+
+    context "when the given attribute has not been declared" do
+      let :attribute, do: :frozz
+
+      it "raises an exception" do
+        expect(fn -> subject() |> IO.inspect end).to(
+          raise_exception(RuntimeError))
+      end
+    end
+  end
+
+  describe "get/3" do
+    subject do: Subject.new()
+      |> Subject.put(:foo, value())
+      |> Subject.get(attribute(), default_value())
+
+    let :value,         do: "foo"
+    let :default_value, do: "frozz"
+
+    context "when the given attribute has been set" do
+      let :attribute, do: :foo
+
+      it "returns the attribute value" do
+        expect(subject()).to eq value()
+      end
+    end
+
+    context "when the given attribute has not been set" do
+      let :attribute, do: :bar
+
+      it "returns the default value" do
+        expect(subject()).to eq default_value()
+      end
+    end
+
+    context "when the given attribute has not been declared" do
+      let :attribute, do: :frozz
+
+      it "raises an exception" do
+        expect(fn -> subject() |> IO.inspect end).to(
+          raise_exception(RuntimeError))
+      end
+    end
+  end
 end
